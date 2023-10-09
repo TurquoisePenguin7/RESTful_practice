bd0e3e46b77703b7a9231b94ce35e84d0fac8095
TurquoisePenguin7 <turquoisehairedpenguinhorn@gmail.com>
2023-10-10 02:02:17 +0300

    changed the argument parsing

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
-import sqlite3
+from flask_restful import Resource, Api, reqparse
+from peewee import SqliteDatabase, Model, CharField
 
-con = sqlite3.connect("test.db")
-cur = con.cursor()
-try:
-    cur.execute("CREATE TABLE tasks(task, status)")
-except sqlite3.OperationalError:
-    print("Table exists, skipping...")
+db = SqliteDatabase('test.db')
+
+class Tasks(Model):
+    task = CharField()
+    status = CharField()
+    
+    class Meta:
+        database = db
+
+db.create_tables([Tasks])
 
 app = Flask(__name__)
 api = Api(app)
 
 class addInfoToDB(Resource):
     def get(self):
-        res = cur.execute("SELECT * FROM tasks")
-        return res.fetchall()
+        return {"tasks": {tasks.task: tasks.status for tasks in Tasks.select()}}, 200
     
-    def put(self, todo, status):
-        cur.execute("INSERT INTO tasks VALUES(?, ?)", todo, status)
-        return {"status": "success"}
+    def post(self):
+        values = reqparse.RequestParser()
+        values.add_argument("task", type=str, default="")
+        values.add_argument("status", type=str, default="")
+        args = values.parse_args()
+        task = Tasks.create(task=args['task'], status=args['status'])
+        task.save()
+        return {'task': args["task"], "status": args["status"]}, 201
     
-class Todo3(Resource):
+class root(Resource):
     def get(self):
         return {"Error": 404, "msg": "Not found :("}, 404
     
@@ -29,8 +37,9 @@ class teapot(Resource):
     def get(self):
         return {"Error": 418, "msg": "I am a little teapot"}, 418
 
-api.add_resource(Todo3, '/')
 api.add_resource(teapot, "/teapot")
+api.add_resource(root, '/')
+api.add_resource(addInfoToDB,"/add")
 
 if __name__ == "__main__":
     app.run(debug=True)
